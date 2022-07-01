533272952ecdbc08dfaf8f0b98ca8eb632e6eac2
mohsenrezaei5367 <mohsenrezaei5367@gmail.com>
2022-07-01 18:27:42 +0430

    commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+
+// login
+let signIn = $('#sign-in-page')
+let signUp = $('#sign-up-page')
+function registerPgee() {
+    signUp.removeClass('none')
+    signIn.addClass('none')
+
+}
+
+function backToLogin() {
+    signUp.addClass('none')
+    signIn.removeClass('none')
+}
+function registerPateint() {
+    signUp.addClass('none')
+    signIn.removeClass('none')
+}
+// end login
+
+
+function loginToDashbord() {
+    window.open("dashboard.html", "_self");
+}
+
+function registerAsPatient() {
+    window.open("register-as-patient.html", "_self");
+}
+function RegisterAsAgent() {
+    window.open("register-as-agent.html", "_self");
+}
+
+//password fild eye
+const eyePass = document.getElementById('eye-password')
+const inputPass = document.getElementById('input-pass')
+if(eyePass){
+   eyePass.addEventListener('click', function(){
+    const type = inputPass.getAttribute('type') === 'password' ? 'text' : 'password'
+    inputPass.setAttribute('type', type)
+})
+      
+}
+
+
+
+
+
+
+  
+
